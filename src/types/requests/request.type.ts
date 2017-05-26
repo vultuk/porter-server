edbe9afd0a84a7abbca50b548fb34c7162115597
Simon Skinner <simon@sisk-ltd.co.uk>
2017-05-26 21:16:59 +0100

    Ability to Proxy requests
    There is now the ability to proxy requests for either single routes
    or for a whole resource. This enables any system to call external
    APIs or other microservices without any extra implementation.
    
    Proxying to another server is as simple as adding a proxyUrl field
    to the appropriate Request object.

--- src/types/requests/request.type.ts
+++ src/types/requests/request.type.ts
@@ -3,6 +3,7 @@ import {Controller} from "../../models/controller.model";
 export class Request {
     endpoint: string;
     method: string;
-    controller: typeof Controller;
+    controller?: typeof Controller;
     action?: string;
+    proxyUrl?: string;
 }
